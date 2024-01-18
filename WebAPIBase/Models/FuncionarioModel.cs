c5ce12453e2a47ba722d7113393a76eca3bb59c0
Leandro Amaral <lramaral02m@gmail.com>
2024-01-18 01:18:44 -0300

    Criando credenciais no appSettings.json

--- WebAPIBase/Models/FuncionarioModel.cs
+++ WebAPIBase/Models/FuncionarioModel.cs
@@ -10,6 +10,7 @@ namespace WebAPIBase.Models
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public DepartamentoEnum Departamento { get; set; }
+        public bool Ativo { get; set; }
         public TurnoEnum Turno { get; set; }
         public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
